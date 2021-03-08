7216e5d4cad1684858e1e722b1013746dc3c72e3
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-03-08 11:24:26 -0300

    apaguei o arquivo antigo e fiz algumas mudanças visuais

--- quiz.js
+++ quiz.js
@@ -5,38 +5,56 @@ function Pergunta(pergunta, op1, op2, op3, resp) {
 
 }
 
-const pergunta_harry           = new Pergunta('Quem é o protagonista do filme harry potter?', 'ronie', 'hermione', 'voldemort', 'harry')
 const pergunta_num_vingadores1 = new Pergunta('Quantos vingadores apareceram no primeiro Avengers?', '4', '5', '7', '6')
+const pergunta_vilao1          = new Pergunta('Qual é o nome do vilão do primeiro Avengers?', 'Thanos', 'Zemo', 'Ultron', 'Loki')
+ 
+const perguntas = [pergunta_num_vingadores1, pergunta_vilao1]
 
-perguntas = [pergunta_harry, pergunta_num_vingadores1]
-
-id_pergunta = 0
-
-pergunta_html = document.querySelector('.frase-pergunta')
-pergunta_html.textContent = perguntas[id_pergunta].pergunta
-
-opcoes_html = document.querySelectorAll('li')
-
-contador = 0
-
-for (let opcao of opcoes_html) {
-    opcao.textContent = perguntas[id_pergunta].alternativas[contador]
-    contador++
+id_pergunta_a_escolher = function(id) {
+    return perguntas[id]
 }
 
-for (let opcao of opcoes_html) {
-    opcao.addEventListener('click', check)
+function rodar_pergunta(id=0) {
+    const pergunta_escolhida = id_pergunta_a_escolher(id)
+    imprimir_a_pergunta(pergunta_escolhida)
 
-    function check() {
-        resultado = document.querySelector('.resultado')
-        if (opcao.textContent == perguntas[id_pergunta].resposta) {
-            resultado.style.display = 'block'
-            resultado.style.color = 'green'
-            resultado.textContent = 'Certa resposta'
-        } else {
-            resultado.style.display = 'block'
-            resultado.style.color = 'red'
-            resultado.textContent = 'Resposta errada'
+    var opcoes_html = document.querySelectorAll('li')
+
+    var contador_alternativas = 0
+    for (let opcao of opcoes_html) {
+        opcao.textContent = pergunta_escolhida.alternativas[contador_alternativas]
+        contador_alternativas++
+    }
+
+    for (let opcao of opcoes_html) {
+        opcao.addEventListener('click', check)
+
+        function check() {
+            resultado = document.querySelector('.resultado')
+            if (opcao.textContent === pergunta_escolhida.resposta) {
+                resultado.style.display = 'block'
+                resultado.style.color = 'green'
+                resultado.textContent = 'Certa resposta'
+            } else {
+                resultado.style.display = 'block'
+                resultado.style.color = 'red'
+                resultado.textContent = 'Resposta errada'
+            }
         }
     }
+}
+
+rodar_pergunta()
+
+const botao_proximo = document.querySelector('.botao-proximo')
+var contador_id = 0 
+botao_proximo.addEventListener('click', function() {
+    contador_id++
+    rodar_pergunta(contador_id)
+    resultado.style.display = 'none'
+})
+
+function imprimir_a_pergunta(pergunta_escolhida) {
+    frase_pergunta_html = document.querySelector('.frase-pergunta')
+    frase_pergunta_html.textContent = pergunta_escolhida.pergunta
 }
