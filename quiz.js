84daa802724c07128a32d9d191355c92102c3b3f
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-03-12 15:05:53 -0300

    Otimização no aparecimento das setas e adicionei perguntas novas

--- quiz.js
+++ quiz.js
@@ -7,12 +7,20 @@ function Pergunta(pergunta, op1, op2, op3, resp) {
 
 const pergunta_num_vingadores1 = new Pergunta('Quantos vingadores apareceram no primeiro Avengers?', '4', '5', '7', '6')
 const pergunta_vilao1          = new Pergunta('Qual é o nome do vilão do primeiro Avengers?', 'Thanos', 'Zemo', 'Ultron', 'Loki')
+const pergunta_vilao2          = new Pergunta('Qual é o nome do vilão do segundo Avengers?', 'Thanos', 'Zemo', 'Loki', 'Ultron')
+const pergunta_strongest       = new Pergunta('Quem é o vingador mais forte de acordo com Tony Stark?', 'Thor', 'Capitão América', 'Homem de Ferro', 'Hulk')
  
-const perguntas = [pergunta_num_vingadores1, pergunta_vilao1]
+const perguntas = [pergunta_num_vingadores1, pergunta_vilao1, pergunta_vilao2, pergunta_strongest]
 
-id_pergunta_a_escolher = function(id) {
+id_pergunta_a_escolher = (id) => {
     return perguntas[id]
 }
+
+var botao_proximo  = document.querySelector('.botao-proximo')
+var botao_anterior = document.querySelector('.botao-anterior')
+
+var contador_id = 0
+
 
 function rodar_pergunta(id=0) {
     const pergunta_escolhida = id_pergunta_a_escolher(id)
@@ -41,31 +49,40 @@ function rodar_pergunta(id=0) {
                 resultado.textContent = 'Resposta errada'
             }
         }
+    } 
+
+    if (contador_id == 0) {
+        botao_anterior.style.display = 'none'
+    } else {
+        botao_anterior.style.display = 'block'
+    }
+
+    if (contador_id == perguntas.length - 1) {
+        botao_proximo.style.display = 'none'
+    } else {
+        botao_proximo.style.display = 'block'
     }
 }
 
 rodar_pergunta()
 
-const botao_proximo  = document.querySelector('.botao-proximo')
-const botao_anterior = document.querySelector('.botao-anterior')
+contador_id = 0 
 
-var contador_id = 0 
 botao_proximo.addEventListener('click', () => {
-    if (contador_id < perguntas.length - 1) {  
-      contador_id++
-    }
+    contador_id++
+
     rodar_pergunta(contador_id)
+
     var resultado = document.querySelector('.resultado')
     resultado.style.display = 'none'
 })
 
 botao_anterior.addEventListener('click', () => {
-    if (contador_id > 0) {
-        contador_id--
-    }
-    rodar_pergunta(contador_id)
-    var resultado = document.querySelector('.resultado')
+    contador_id--
 
+    rodar_pergunta(contador_id)
+
+    var resultado = document.querySelector('.resultado')
     resultado.style.display = 'none'
 })
 
@@ -73,4 +90,3 @@ function imprimir_a_pergunta(pergunta_escolhida) {
     frase_pergunta_html = document.querySelector('.frase-pergunta')
     frase_pergunta_html.textContent = pergunta_escolhida.pergunta
 }
-
