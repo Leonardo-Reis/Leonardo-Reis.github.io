38e1c71fa79ef614118bdbf49bab9f864c60b922
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-03-15 19:15:08 -0300

    O novo codigo passou a ser o padrão e apaguei o codigo antigo

--- quiz.js
+++ quiz.js
@@ -1,87 +1,102 @@
-function Pergunta(pergunta, op1, op2, op3, resp) {
+function Pergunta (pergunta, op1, op2, op3, resp) {
     this.alternativas = [op1, op2, op3, resp].sort()
-    this.resposta = resp
-    this.pergunta = pergunta
-
+    this.resposta   = resp
+    this.pergunta   = pergunta
+    this.respondida = false
+    this.escolhida  = ''
 }
 
 const pergunta_num_vingadores1 = new Pergunta('Quantos vingadores apareceram no primeiro Avengers?', '4', '5', '7', '6')
 const pergunta_vilao1          = new Pergunta('Qual é o nome do vilão do primeiro Avengers?', 'Thanos', 'Zemo', 'Ultron', 'Loki')
 const pergunta_vilao2          = new Pergunta('Qual é o nome do vilão do segundo Avengers?', 'Thanos', 'Zemo', 'Loki', 'Ultron')
 const pergunta_strongest       = new Pergunta('Quem é o vingador mais forte de acordo com Tony Stark?', 'Thor', 'Capitão América', 'Homem de Ferro', 'Hulk')
- 
-const perguntas = [pergunta_num_vingadores1, pergunta_vilao1, pergunta_vilao2, pergunta_strongest]
 
-id_pergunta_a_escolher = (id) => {
-    return perguntas[id]
-}
+const quiz = {
+    perguntas: [pergunta_num_vingadores1, pergunta_vilao1, pergunta_vilao2, pergunta_strongest],
 
-var botao_proximo  = document.querySelector('.botao-proximo')
-var botao_anterior = document.querySelector('.botao-anterior')
+    opcoes_html: document.querySelectorAll('li'),
+    resultado: document.querySelector('.resultado'),
+    botao_proximo: document.querySelector('.botao-proximo'),
+    botao_anterior: document.querySelector('.botao-anterior'),
 
-var contador_id = 0
+    contador_id: 0,
+    contador_rodadas: 0,
 
-function rodar_pergunta(id=0) {
-    const pergunta_escolhida = id_pergunta_a_escolher(id)
-    imprimir_a_pergunta(pergunta_escolhida)
+    init: function (contador_id=0) {
+        this.imprimir_pergunta(contador_id)
+        this.imprimir_alternativas(contador_id) 
+        this.setar_aparição_das_setas(contador_id)
 
-    var opcoes_html = document.querySelectorAll('li')
-
-    opcoes_html.forEach((opcao, contador_alternativas=0) => {opcao.textContent = pergunta_escolhida.alternativas[contador_alternativas++]})
-
-    opcoes_html.forEach((opcao) => {
-        opcao.addEventListener('click', check)
-
-        function check() {
-            var resultado = document.querySelector('.resultado')
-            if (opcao.textContent === pergunta_escolhida.resposta) {
-                resultado.style.display = 'block'
-                resultado.style.color = 'green'
-                resultado.textContent = 'Certa resposta'
-            } else {
-                resultado.style.display = 'block'
-                resultado.style.color = 'red'
-                resultado.textContent = 'Resposta errada'
-            }
+        if (this.contador_rodadas === 0) {
+            this.adicionar_listeners_setas()
         }
-    })
 
-    if (contador_id == 0) {
-        botao_anterior.style.display = 'none'
-    } else {
-        botao_anterior.style.display = 'block'
-    }
 
-    if (contador_id == perguntas.length - 1) {
-        botao_proximo.style.display = 'none'
-    } else {
-        botao_proximo.style.display = 'block'
-    }
-}
+        this.contador_rodadas++
+    },
 
-rodar_pergunta()
+    adicionar_listeners_setas: function () {
+        this.botao_proximo.addEventListener('click', () => {
+            if (this.contador_id < this.perguntas.length - 1){
+                this.contador_id++
+            }
 
-var contador_id = 0 
+            this.resultado.style.display = 'none'
 
-botao_proximo.addEventListener('click', () => {
-    contador_id++
+            this.init(this.contador_id)
+        })
 
-    rodar_pergunta(contador_id)
+        this.botao_anterior.addEventListener('click', () => {
+            if (this.contador_id > 0) {
+                this.contador_id--
+            }
 
-    var resultado = document.querySelector('.resultado')
-    resultado.style.display = 'none'
-})
+            this.resultado.style.display = 'none'
 
-botao_anterior.addEventListener('click', () => {
-    contador_id--
+            this.init(this.contador_id)
+        })
 
-    rodar_pergunta(contador_id)
+    },
 
-    var resultado = document.querySelector('.resultado')
-    resultado.style.display = 'none'
-})
+    setar_aparição_das_setas: function (contador_id) {
+        if (contador_id == 0) {
+            this.botao_anterior.style.display = 'none'
+        } else {
+            this.botao_anterior.style.display = 'block'
+        }
+        if (contador_id == this.perguntas.length - 1) {
+            this.botao_proximo.style.display = 'none'
+        } else {
+            this.botao_proximo.style.display = 'block'
+        }
+    },
 
-function imprimir_a_pergunta(pergunta_escolhida) {
-    frase_pergunta_html = document.querySelector('.frase-pergunta')
-    frase_pergunta_html.textContent = pergunta_escolhida.pergunta
+    imprimir_alternativas: function (id=0) {
+        this.opcoes_html.forEach((opcao, id_alternativa=0) => {
+            let pergunta_atual = this.perguntas[id]
+
+            opcao.textContent = pergunta_atual.alternativas[id_alternativa]
+
+            opcao.addEventListener('click', () => {
+                pergunta_atual.respondida = true
+
+                pergunta_atual.escolhida = opcao
+
+                this.resultado.style.display = 'block'
+                if (pergunta_atual.escolhida.textContent == pergunta_atual.resposta) {
+                    this.resultado.textContent = 'Resposta certa!'
+                    this.resultado.style.color = 'green'
+                } else {
+                    this.resultado.textContent = 'Resposta errada...'
+                    this.resultado.style.color = 'red'
+                }
+            })
+        })
+    },
+
+    imprimir_pergunta: function (id=0) {
+        frase_pergunta_html = document.querySelector('.frase-pergunta')
+        
+        frase_pergunta_html.textContent = this.perguntas[id].pergunta
+    },
 }
